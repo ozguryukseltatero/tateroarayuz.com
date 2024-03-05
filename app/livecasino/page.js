035896fff4b76ec7be34d170b296dff9bc4c19d5
ozguryukseltatero <161950739+ozguryukseltatero@users.noreply.github.com>
2024-03-05 17:56:28 +0300

    Add files via upload

---
+++ app/livecasino/page.js
@@ -0,0 +1,21 @@
+import Footer from "@/components/footer/Footer";
+import SubHeader from "@/components/header/SubHeader";
+import LivecasinoMain from "@/components/livecasino/LivecasinoMain";
+import NavBar from "@/components/navBar/NavBar";
+
+export default function Livecasino() {
+  return (
+    <>
+      {/* Nav Bar */}
+      <NavBar />
+
+      
+
+      {/* Livecasino Main */}
+      <LivecasinoMain />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
